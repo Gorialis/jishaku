b47e41019310cac14f919832e00384f52076d5e1
Gorialis <sansgorialis@gmail.com>
2017-11-28 05:32:19 +0900

    Module and license cleanup

--- jishaku/__init__.py
+++ jishaku/__init__.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 
+"""
+Jishaku
+~~~~~~~
+
+A debugging, testing and experimentation cog for Discord bots.
+
+:copyright: (c) 2017 Devon R
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'jishaku'
+__author__ = 'Gorialis'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Devon R'
 __version__ = '0.0.2'
+
 
 from . import cog
 
