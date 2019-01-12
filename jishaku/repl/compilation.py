Gorialis/jishaku

commit 706674c7efb527f930072aaf896a402402c8c869

Prevent REPL from dying on empty/comment-only evals

jishaku/repl/compilation.py
@@ -33,6 +33,7 @@ async def _repl_coroutine({{0}}):
     import jishaku
 
     try:
+        pass
 {{1}}
     finally:
         _async_executor = jishaku.repl.get_parent_var('async_executor', skip_frames=1)
@@ -50,7 +51,7 @@ def wrap_code(code: str, args: str = '') -> ast.Module:
 
     if sys.version_info >= (3, 7):
         user_code = import_expression.parse(code, mode='exec')
-        injected = 'pass'
+        injected = ''
     else:
         injected = code
 
@@ -63,7 +64,7 @@ def wrap_code(code: str, args: str = '') -> ast.Module:
     assert isinstance(try_block, ast.Try)
 
     if sys.version_info >= (3, 7):
-        try_block.body = user_code.body
+        try_block.body.extend(user_code.body)
     else:
         ast.increment_lineno(mod, -12)  # bring line numbers back in sync with repl
 

docs/conf.py
@@ -19,14 +19,16 @@
 
 # -- Project information -----------------------------------------------------
 
+from jishaku.meta import __version__
+
 project = 'jishaku'
 copyright = '2019, Gorialis'
 author = 'Gorialis'
 
 # The short X.Y version
-version = '1.3.4'
+version = __version__
 # The full version, including alpha/beta/rc tags
-release = '1.3.4'
+release = version
 
 
 # -- General configuration ---------------------------------------------------

jishaku/meta.py
@@ -25,4 +25,4 @@ __copyright__ = 'Copyright 2019 Devon (Gorialis) R'
 __docformat__ = 'restructuredtext en'
 __license__ = 'MIT'
 __title__ = 'jishaku'
-__version__ = '1.3.4'
+__version__ = '1.3.5'

tests/test_repl.py
@@ -30,7 +30,7 @@ class ReplInternalsTest(unittest.TestCase):
             del hidden_variable
 
             test = self.upper_method()
-            self.assertEqual(test, None)
+            self.assertIsNone(test)
 
             self.assertEqual(get_parent_var('unittest', global_ok=True), unittest)
 
@@ -184,3 +184,17 @@ class ReplAsyncExecutorTest(unittest.TestCase):
 
         self.assertEqual(len(return_data), 1, msg="Checking await returns result")
         self.assertEqual(return_data[0], 22, msg="Checking await result consistent")
+
+        return_data = []
+        async for result in AsyncCodeExecutor("", scope):
+            return_data.append(result)
+
+        self.assertEqual(len(return_data), 1, msg="Checking empty eval returns")
+        self.assertIsNone(return_data[0], msg="Checking empty eval returns None")
+
+        return_data = []
+        async for result in AsyncCodeExecutor("# this is a comment", scope):
+            return_data.append(result)
+
+        self.assertEqual(len(return_data), 1, msg="Checking eval of only comment returns")
+        self.assertIsNone(return_data[0], msg="Checking eval of only comment returns None")

commit adb9fb0a94ea41d43e4d751f2163da924419cb05

REPL compilation: fix linenos of user code on py3.6
Commit 706674c7efb527f930072aaf896a402402c8c869 introduced a regression:
on python3.6, errors in usercode now start at line #2, not line #1.
This is due to a constant offset, -12, being passed to ast.increment_linenos.
When that commit added a `pass` line, the -12 should've turned into a -13, but that seems to have been forgotten.

Rather than just manually update the magic number, this commit computes it at runtime.

jishaku/repl/compilation.py
@@ -21,7 +21,7 @@ import import_expression
 
 from .scope import Scope
 
-CORO_CODE = """
+CORO_CODE_PRELUDE = """
 async def _repl_coroutine({{0}}):
     import asyncio
     from importlib import import_module as {0}
@@ -34,13 +34,14 @@ async def _repl_coroutine({{0}}):
 
     try:
         pass
-{{1}}
+""".format(import_expression.constants.IMPORTER)
+
+CORO_CODE_POSTLUDE = """
     finally:
         _async_executor = jishaku.repl.get_parent_var('async_executor', skip_frames=1)
         if _async_executor:
             _async_executor.scope.globals.update(locals())
-""".format(import_expression.constants.IMPORTER)
-
+"""
 
 def wrap_code(code: str, args: str = '') -> ast.Module:
     """
@@ -53,9 +54,10 @@ def wrap_code(code: str, args: str = '') -> ast.Module:
         user_code = import_expression.parse(code, mode='exec')
         injected = ''
     else:
-        injected = code
+        injected = textwrap.indent(code, ' ' * 8)
 
-    mod = import_expression.parse(CORO_CODE.format(args, textwrap.indent(injected, ' ' * 8)), mode='exec')
+    coro_code = CORO_CODE_PRELUDE + injected + CORO_CODE_POSTLUDE
+    mod = import_expression.parse(coro_code.format(args), mode='exec')
 
     definition = mod.body[-1]  # async def ...:
     assert isinstance(definition, ast.AsyncFunctionDef)
@@ -66,7 +68,7 @@ def wrap_code(code: str, args: str = '') -> ast.Module:
     if sys.version_info >= (3, 7):
         try_block.body.extend(user_code.body)
     else:
-        ast.increment_lineno(mod, -12)  # bring line numbers back in sync with repl
+        ast.increment_lineno(mod, -1 * CORO_CODE_PRELUDE.count('\n'))  # bring line numbers back in sync with repl
 
     ast.fix_missing_locations(mod)
 