51d73d11084562aff09c2d2b03abeb05b866095c
Devon R <sansgorialis@gmail.com>
2019-03-02 20:27:41 +0900

    Requirements reshuffle

--- setup.py
+++ setup.py
@@ -32,10 +32,10 @@ from setuptools import setup
 
 ROOT = pathlib.Path(__file__).parent
 
-with open(str(ROOT / 'requirements.txt'), 'r', encoding='utf-8') as f:
+with open(ROOT / 'requirements' / 'pypi.txt', 'r', encoding='utf-8') as f:
     REQUIREMENTS = f.read().splitlines()
 
-with open(str(ROOT / 'jishaku' / 'meta.py'), 'r', encoding='utf-8') as f:
+with open(ROOT / 'jishaku' / 'meta.py', 'r', encoding='utf-8') as f:
     VERSION = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 if not VERSION:
@@ -61,65 +61,73 @@ if VERSION.endswith(('a', 'b', 'rc')):
         pass
 
 
-with open(str(ROOT / 'README.rst'), 'r', encoding='utf-8') as f:
+with open(ROOT / 'README.rst', 'r', encoding='utf-8') as f:
     README = f.read()
 
 
-setup(name='jishaku',
-      author='Devon (Gorialis) R',
-      url='https://github.com/Gorialis/jishaku',
+setup(
+    name='jishaku',
+    author='Devon (Gorialis) R',
+    url='https://github.com/Gorialis/jishaku',
 
-      license='MIT',
-      description='A discord.py extension including useful tools for bot development and debugging.',
-      long_description=README,
-      long_description_content_type='text/x-rst',
+    license='MIT',
+    description='A discord.py extension including useful tools for bot development and debugging.',
+    long_description=README,
+    long_description_content_type='text/x-rst',
+    project_urls={
+        'Documentation': 'https://jishaku.readthedocs.io/en/latest/',
+        'Code': 'https://github.com/Gorialis/jishaku',
+        'Issue tracker': 'https://github.com/Gorialis/jishaku/issues'
+    },
 
-      version=VERSION,
-      packages=['jishaku', 'jishaku.repl'],
-      include_package_data=True,
-      install_requires=REQUIREMENTS,
-      python_requires='>=3.6.0',
+    version=VERSION,
+    packages=['jishaku', 'jishaku.repl'],
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    python_requires='>=3.6.0',
 
-      extras_require={
-          'docs': [
-              'sphinx>=1.7.0',
-              'sphinxcontrib-asyncio'
-          ],
+    extras_require={
+        'docs': [
+            'sphinx>=1.7.0',
+            'sphinxcontrib-asyncio'
+        ],
 
-          'test': [
-              'coverage',
-              'flake8',
-              'isort',
-              'pylint',
-              'pytest',
-              'pytest-cov'
-          ],
+        'test': [
+            'coverage',
+            'flake8',
+            'isort',
+            'pylint',
+            'pytest',
+            'pytest-cov'
+        ],
 
-          'voice': [
-              'PyNaCl',
-              'youtube-dl'
-          ],
+        'voice': [
+            'PyNaCl',
+            'youtube-dl'
+        ],
 
-          'procinfo': [
-              'psutil'
-          ]
-      },
+        'procinfo': [
+            'psutil'
+        ]
+    },
 
-      download_url='https://github.com/Gorialis/jishaku/archive/{}.tar.gz'.format(VERSION),
+    download_url='https://github.com/Gorialis/jishaku/archive/{}.tar.gz'.format(VERSION),
 
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Framework :: AsyncIO',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Natural Language :: English',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: 3 :: Only',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Topic :: Communications :: Chat',
-          'Topic :: Internet',
-          'Topic :: Software Development :: Debuggers',
-          'Topic :: Software Development :: Testing',
-          'Topic :: Utilities'
-      ])
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: AsyncIO',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Communications :: Chat',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities'
+    ]
+)
