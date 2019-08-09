Gorialis/jishaku

commit b3bf5dbb22e370aa9c8341ba9d0efc787ccfa892

Add KeywordTransformer to solve a variety of keyword-related problems
- `del x` should now work as intended for deleting variables defined in
retain mode
- `return` and `yield` mixing is now possible, and correctly accounted
for.
- A `return`/`yield` mixing error will no longer produce an exception if
a yield is contained within a nested function. Likewise, nested
functions now remain unaffected by the above transformations.

jishaku/repl/walkers.py
@@ -1,0 +1,189 @@
+# -*- coding: utf-8 -*-
+
+"""
+jishaku.repl.walkers
+~~~~~~~~~~~~~~~~~~~~
+
+AST walkers for code transformation and analysis.
+
+:copyright: (c) 2019 Devon (Gorialis) R
+:license: MIT, see LICENSE for more details.
+
+"""
+
+import ast
+
+# pylint: disable=no-self-use,invalid-name,missing-docstring
+
+
+class KeywordTransformer(ast.NodeTransformer):
+    """
+    This transformer:
+    - Converts return-with-value into yield & return
+    - Converts bare deletes into conditional global pops
+    """
+
+    def visit_FunctionDef(self, node):
+        # Do not affect nested function definitions
+        return node
+
+    def visit_AsyncFunctionDef(self, node):
+        # Do not affect nested async function definitions
+        return node
+
+    def visit_ClassDef(self, node):
+        # Do not affect nested class definitions
+        return node
+
+    def visit_Return(self, node):
+        # Do not modify valueless returns
+        if node.value is None:
+            return node
+
+        # Otherwise, replace the return with a yield & valueless return
+        return ast.If(
+            test=ast.NameConstant(
+                value=True,  # if True; aka unconditional, will be optimized out
+                lineno=node.lineno,
+                col_offset=node.col_offset
+            ),
+            body=[
+                # yield the value to be returned
+                ast.Expr(
+                    value=ast.Yield(
+                        value=node.value,
+                        lineno=node.lineno,
+                        col_offset=node.col_offset
+                    ),
+                    lineno=node.lineno,
+                    col_offset=node.col_offset
+                ),
+                # return valuelessly
+                ast.Return(
+                    value=None,
+                    lineno=node.lineno,
+                    col_offset=node.col_offset
+                )
+            ],
+            orelse=[],
+            lineno=node.lineno,
+            col_offset=node.col_offset
+        )
+
+    def visit_Delete(self, node):
+        """
+        This converter replaces bare deletes with conditional global pops.
+
+        It is roughly equivalent to transforming:
+
+        .. code:: python
+
+            del foobar
+
+        into:
+
+        .. code:: python
+
+            if 'foobar' in globals():
+                globals().pop('foobar')
+            else:
+                del foobar
+
+        This thus makes deletions in retain mode work more-or-less as intended.
+        """
+
+        return ast.If(
+            test=ast.NameConstant(
+                value=True,  # if True; aka unconditional, will be optimized out
+                lineno=node.lineno,
+                col_offset=node.col_offset
+            ),
+            body=[
+                ast.If(
+                    # if 'x' in globals():
+                    test=ast.Compare(
+                        # 'x'
+                        left=ast.Str(
+                            s=target.id,
+                            lineno=node.lineno,
+                            col_offset=node.col_offset
+                        ),
+                        ops=[
+                            # in
+                            ast.In(
+                                lineno=node.lineno,
+                                col_offset=node.col_offset
+                            )
+                        ],
+                        comparators=[
+                            # globals()
+                            self.globals_call(node)
+                        ],
+                        lineno=node.lineno,
+                        col_offset=node.col_offset
+                    ),
+                    body=[
+                        ast.Expr(
+                            # globals().pop('x')
+                            value=ast.Call(
+                                # globals().pop
+                                func=ast.Attribute(
+                                    value=self.globals_call(node),
+                                    attr='pop',
+                                    ctx=ast.Load(),
+                                    lineno=node.lineno,
+                                    col_offset=node.col_offset
+                                ),
+                                args=[
+                                    # 'x'
+                                    ast.Str(
+                                        s=target.id,
+                                        lineno=node.lineno,
+                                        col_offset=node.col_offset
+                                    )
+                                ],
+                                keywords=[],
+                                lineno=node.lineno,
+                                col_offset=node.col_offset
+                            ),
+                            lineno=node.lineno,
+                            col_offset=node.col_offset
+                        )
+                    ],
+                    # else:
+                    orelse=[
+                        # del x
+                        ast.Delete(
+                            targets=[target],
+                            lineno=node.lineno,
+                            col_offset=node.col_offset
+                        )
+                    ],
+                    lineno=node.lineno,
+                    col_offset=node.col_offset
+                )
+                # for each target to be deleted, e.g. `del {x}, {y}, {z}`
+                for target in node.targets
+            ],
+            orelse=[],
+            lineno=node.lineno,
+            col_offset=node.col_offset
+        )
+
+    def globals_call(self, node):
+        """
+        Creates an AST node that calls globals().
+        """
+
+        return ast.Call(
+            func=ast.Name(
+                id='globals',
+                ctx=ast.Load(),
+                lineno=node.lineno,
+                col_offset=node.col_offset
+            ),
+            args=[],
+            keywords=[],
+            lineno=node.lineno,
+            col_offset=node.col_offset
+        )

jishaku/repl/compilation.py
@@ -21,6 +21,7 @@ import import_expression
 
 from jishaku.functools import AsyncSender
 from jishaku.repl.scope import Scope
+from jishaku.repl.walkers import KeywordTransformer
 
 CORO_CODE = """
 async def _repl_coroutine({{0}}):
@@ -75,7 +76,7 @@ def wrap_code(code: str, args: str = '') -> ast.Module:
 
     ast.fix_missing_locations(mod)
 
-    is_asyncgen = any(isinstance(node, ast.Yield) for node in ast.walk(try_block))
+    KeywordTransformer().generic_visit(try_block)
 
     last_expr = try_block.body[-1]
 
@@ -85,24 +86,15 @@ def wrap_code(code: str, args: str = '') -> ast.Module:
 
     # if the last expression is not a yield
     if not isinstance(last_expr.value, ast.Yield):
-        # copy the expression into a return/yield
-        if is_asyncgen:
-            # copy the value of the expression into a yield
-            yield_stmt = ast.Yield(last_expr.value)
-            ast.copy_location(yield_stmt, last_expr)
-            # place the yield into its own expression
-            yield_expr = ast.Expr(yield_stmt)
-            ast.copy_location(yield_expr, last_expr)
+        # copy the value of the expression into a yield
+        yield_stmt = ast.Yield(last_expr.value)
+        ast.copy_location(yield_stmt, last_expr)
+        # place the yield into its own expression
+        yield_expr = ast.Expr(yield_stmt)
+        ast.copy_location(yield_expr, last_expr)
 
-            # place the yield where the original expression was
-            try_block.body[-1] = yield_expr
-        else:
-            # copy the expression into a return
-            return_stmt = ast.Return(last_expr.value)
-            ast.copy_location(return_stmt, last_expr)
-
-            # place the return where the original expression was
-            try_block.body[-1] = return_stmt
+        # place the yield where the original expression was
+        try_block.body[-1] = yield_expr
 
     return mod
 

commit aeade2db288322eb3bc177357fc1fe19f1f4da8a

KeywordTransformer: allow `del x.y, z[1]`
b3bf5db introduced a regression: since visit_Delete assumes that all deletion targets are names,
it fails on non names, ie attribute access and subscription. This commit fixes that by turning e.g.

```
del a, b.c, d[1]
```

into the equivalent of
```
if True:
	if 'a' in globals():
		globals().pop('a')
	else:
		del a
	del b.c
	del d[1]
```
ie it passes through non-names unmodified.

jishaku/repl/walkers.py
@@ -92,13 +92,20 @@ class KeywordTransformer(ast.NodeTransformer):
         This thus makes deletions in retain mode work more-or-less as intended.
         """
 
-        return ast.If(
-            test=ast.NameConstant(
-                value=True,  # if True; aka unconditional, will be optimized out
-                lineno=node.lineno,
-                col_offset=node.col_offset
-            ),
-            body=[
+        body = []
+        for target in node.targets:  # for each target to be deleted, e.g. `del {x}, {y.z}, {z[1]}`
+            if not isinstance(target, ast.Name):
+                # if it's a subscript or attribute access, pass it through unmodified
+                body.append(
+                    ast.Delete(
+                        targets=[target],
+                        lineno=node.lineno,
+                        col_offset=node.col_offset
+                    )
+                )
+                continue
+
+            body.append(
                 ast.If(
                     # if 'x' in globals():
                     test=ast.Compare(
@@ -162,9 +169,15 @@ class KeywordTransformer(ast.NodeTransformer):
                     lineno=node.lineno,
                     col_offset=node.col_offset
                 )
-                # for each target to be deleted, e.g. `del {x}, {y}, {z}`
-                for target in node.targets
-            ],
+            )
+
+        return ast.If(
+            test=ast.NameConstant(
+                value=True,  # if True; aka unconditional, will be optimized out
+                lineno=node.lineno,
+                col_offset=node.col_offset
+            ),
+            body=body,
             orelse=[],
             lineno=node.lineno,
             col_offset=node.col_offset