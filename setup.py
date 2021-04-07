a501652643781fdc543537a0233e168255bef318
Devon R <sansgorialis@gmail.com>
2021-04-07 21:20:35 +0900

    Drop 3.7 support (3.8+ only now)

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
     packages=['jishaku', 'jishaku.features', 'jishaku.repl'],
     include_package_data=True,
     install_requires=REQUIREMENTS,
-    python_requires='>=3.7.0',
+    python_requires='>=3.8.0',
 
     extras_require=EXTRA_REQUIRES,
 
@@ -116,7 +116,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Topic :: Communications :: Chat',
