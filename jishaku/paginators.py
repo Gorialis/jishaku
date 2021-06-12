43f135c2eea5ec507c373674f769df12053d85da
Devon R <sansgorialis@gmail.com>
2021-06-12 14:32:13 +0900

    Abide by the old owner interface

--- jishaku/paginators.py
+++ jishaku/paginators.py
@@ -296,38 +296,59 @@ class PaginatorInterface(ui.View):  # pylint: disable=too-many-instance-attribut
             else:
                 await self.message.edit(view=None)
 
+    def interaction_check(self, user):
+        return not self.owner or user.id == self.owner.id
+
     @ui.button(label="1 \u200b \N{BLACK LEFT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR}", style=discord.ButtonStyle.secondary)
     async def button_start(self, button: ui.Button, interaction: discord.Interaction):  # pylint: disable=unused-argument
         """Button to send interface to first page"""
+        if not self.interaction_check(interaction.user):
+            return
+
         self._display_page = 0
         self.interaction_lock.set()
 
     @ui.button(label="\N{BLACK LEFT-POINTING TRIANGLE}", style=discord.ButtonStyle.secondary)
     async def button_previous(self, button: ui.Button, interaction: discord.Interaction):  # pylint: disable=unused-argument
         """Button to send interface to previous page"""
+        if not self.interaction_check(interaction.user):
+            return
+
         self._display_page -= 1
         self.interaction_lock.set()
 
     @ui.button(label="1", style=discord.ButtonStyle.primary)
     async def button_current(self, button: ui.Button, interaction: discord.Interaction):  # pylint: disable=unused-argument
         """Button to refresh the interface"""
+        if not self.interaction_check(interaction.user):
+            return
+
         self.interaction_lock.set()
 
     @ui.button(label="\N{BLACK RIGHT-POINTING TRIANGLE}", style=discord.ButtonStyle.secondary)
     async def button_next(self, button: ui.Button, interaction: discord.Interaction):  # pylint: disable=unused-argument
         """Button to send interface to next page"""
+        if not self.interaction_check(interaction.user):
+            return
+
         self._display_page += 1
         self.interaction_lock.set()
 
     @ui.button(label="\N{BLACK RIGHT-POINTING DOUBLE TRIANGLE WITH VERTICAL BAR} \u200b 1", style=discord.ButtonStyle.secondary)
     async def button_last(self, button: ui.Button, interaction: discord.Interaction):  # pylint: disable=unused-argument
         """Button to send interface to last page"""
+        if not self.interaction_check(interaction.user):
+            return
+
         self._display_page = self.page_count - 1
         self.interaction_lock.set()
 
     @ui.button(label="\N{BLACK SQUARE FOR STOP} \u200b Close paginator", style=discord.ButtonStyle.danger)
     async def button_close(self, button: ui.Button, interaction: discord.Interaction):  # pylint: disable=unused-argument
         """Button to close the interface"""
+        if not self.interaction_check(interaction.user):
+            return
+
         message = self.message
         self.message = None
         self.task.cancel()
