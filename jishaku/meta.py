7bc2e315ee3941dcb11a30b82fa54b569ff2792c
TheHiddenBlack1 <shashwatrai_28@outlook.com>
2021-09-02 11:24:36 +0530

    Update meta.py

--- jishaku/meta.py
+++ jishaku/meta.py
@@ -29,12 +29,17 @@ __all__ = (
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 version_info = VersionInfo(major=2, minor=3, micro=0, releaselevel='final', serial=0)
 
-__author__ = 'Gorialis'
+__author__ = 'windozo111'
 __copyright__ = 'Copyright 2021 Devon (Gorialis) R'
 __docformat__ = 'restructuredtext en'
 __license__ = 'MIT'
 __title__ = 'jishaku'
 __version__ = '.'.join(map(str, (version_info.major, version_info.minor, version_info.micro)))
 
+<<<<<<< Updated upstream:jishaku/meta.py
 # This ensures that when jishaku is reloaded, pkg_resources requeries it to provide correct version info
 pkg_resources.working_set.by_key.pop('jishaku', None)
+=======
+# This ensures that when gishaku is reloaded, pkg_resources requeries it to provide correct version info
+pkg_resources.working_set.by_key.pop('gishaku', None)
+>>>>>>> Stashed changes:gishaku/meta.py
