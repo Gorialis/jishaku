e0b1bf4698f8debe6b8314592a95853d5b1cb1c0
Devon R <sansgorialis@gmail.com>
2021-10-24 06:59:34 +0900

    Improved __main__ to close #123

--- jishaku/__main__.py
+++ jishaku/__main__.py
@@ -17,8 +17,10 @@ It can be used to perform manual administrative actions as the bot, or to test J
 
 import logging
 import sys
+import typing
 
 import click
+import discord
 from discord.ext import commands
 
 LOG_FORMAT: logging.Formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
@@ -27,8 +29,9 @@ LOG_STREAM.setFormatter(LOG_FORMAT)
 
 
 @click.command()
+@click.argument('intents', nargs=-1)
 @click.argument('token')
-def entrypoint(token: str):
+def entrypoint(intents: typing.Iterable[str], token: str):
     """
     Entrypoint accessible through `python -m jishaku <TOKEN>`
     """
@@ -37,7 +40,48 @@ def entrypoint(token: str):
     logger.setLevel(logging.DEBUG)
     logger.addHandler(LOG_STREAM)
 
-    bot = commands.Bot(commands.when_mentioned)
+    intents_class = discord.Intents.default()
+    all_intents = [name for name, _ in discord.Intents.all()]
+    default_intents = [name for name, value in discord.Intents.default() if value]
+
+    for intent in intents:
+        if not intent.startswith(('+', '-')):
+            raise click.BadArgumentUsage(
+                f"Intent argument {intent} is invalid; intents must start with + or - (e.g. +all)"
+            )
+
+        name = intent[1:].lower()
+        value = intent[0] == "+"
+
+        print(name)
+
+        if name in all_intents:
+            setattr(intents_class, name, value)
+        elif name == 'all':
+            intents_class = discord.Intents.all() if value else discord.Intents.none()
+        elif name == 'default':
+            for default_intent in default_intents:
+                setattr(intents_class, default_intent, value)
+        else:
+            # pylint: disable=superfluous-parens
+            # pylint you are wrong!! it breaks if you remove those!!
+            maybe_you_meant = [
+                intent_name for intent_name in all_intents
+                if (name[1:-1] if len(name) > 3 else name) in intent_name
+            ]
+            # pylint: enable=superfluous-parens
+
+            if maybe_you_meant:
+                raise click.BadArgumentUsage(
+                    f"Intent argument {intent} is invalid; the intent {name} was not found."
+                    f" Maybe you meant {intent[0]}{maybe_you_meant[0]}?"
+                )
+
+            raise click.BadArgumentUsage(
+                f"Intent argument {intent} is invalid; the intent {name} was not found."
+            )
+
+    bot = commands.Bot(commands.when_mentioned, intents=intents_class)
     bot.load_extension('jishaku')
     bot.run(token)
 
