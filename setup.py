af743a37ae2ef10174e05559723d2b6a0c0bd96f
Devon R <sansgorialis@gmail.com>
2023-10-15 04:02:32 +0900

    Revert MSPV to 3.8

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ setup(
     packages=['jishaku', 'jishaku.features', 'jishaku.repl'],
     include_package_data=True,
     install_requires=REQUIREMENTS,
-    python_requires='>=3.10.0',
+    python_requires='>=3.8.0',
 
     extras_require=EXTRA_REQUIRES,
     entry_points={
@@ -138,8 +138,11 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Communications :: Chat',
         'Topic :: Internet',
         'Topic :: Software Development :: Debuggers',
